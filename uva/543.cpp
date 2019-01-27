88d6f1b2966df1d66bea7b3ea5aa946c9260aef2
Lester <lester@ug.uchile.cl>
2019-01-27 17:00:06 -0300

    uva 543

---
+++ uva/543.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <complex>
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+
+using namespace std;
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	
+	// cin.ignore(); must be there when using getline(cin, s)
+	long long x = 1000001;
+	std::vector<int> v(x,0);
+	for (ll i = 2; i < x; ++i) {
+		if (v[i] > 0) {
+			continue;
+		} else {
+			
+			for (ll j = i * i; j < x; j += i) {
+				v[j] = 1;
+			}
+		}
+	}
+	int n;
+	while(1)
+	{
+		cin >> n;
+		if (n == 0) {
+			break;
+		}
+		for (int i = 3; i < n-2; ++i)
+		{
+			if (v[i] == 0 && v[n-i] == 0) {
+				printf("%d = %d + %d\n", n, i, n-i);
+				break;
+			}
+		}
+		
+	}
+	return 0;
+}
