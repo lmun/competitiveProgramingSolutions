5ef9ef470fe8b3198e4174a63602c6720cf6985a
Lester <lester.munoz@gmail.com>
2019-09-27 10:36:08 -0300

    codeforces 1208 h

---
+++ codeforces/1208/h.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define endl "\n"
+#define ll long long int
+#define vi vector<ll>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pii pair<int,int>
+#define vpii vector<pii >
+#define wgraph vector<vpii >
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001
+#define all(c) c.begin(),c.end()
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define f first
+#define s second
+#define LSOne(S) ((S) & -(S))
+
+
+using namespace std;
+
+
+class FenwickTree {                    // remember that index 0 is not used
+private: vi ft; int n;        // recall that vi is: typedef vector<int> vi;
+public: FenwickTree(int _n) : n(_n) { ft.assign(n+1, 0); }    // n+1 zeroes
+  FenwickTree(const vi& f) : n(f.size()-1) { ft.assign(n+1, 0);
+    for (int i = 1; i <= n; i++) {                                  // O(n)
+      ft[i] += f[i];                                      // add this value
+      if (i+LSOne(i) <= n)    // if index i has parent in the updating tree
+        ft[i+LSOne(i)] += ft[i]; } }       // add this value to that parent
+  ll rsq(int j) {                                     // returns RSQ(1, j)
+    ll sum = 0; for (; j; j -= LSOne(j)) sum += ft[j];
+    return sum; }
+  ll rsq(int i, int j) { return rsq(j) - rsq(i-1); }  // returns RSQ(i, j)
+  // updates value of the i-th element by v (v can be +ve/inc or -ve/dec)
+  void update(int i, ll v) {
+    for (; i <= n; i += LSOne(i)) ft[i] += v; }    // note: n = ft.size()-1
+  int select(ll k) { // O(log^2 n)
+    int lo = 1, hi = n;
+    while(hi>lo) { // 2^30 > 10^9 > usual Fenwick Tree size
+      int mid = (lo+hi) / 2;                    // Binary Search the Answer
+      (rsq(mid) <= k) ? lo = mid+1 : hi = mid;}
+    return hi; }
+};
+
+class RUPQ : FenwickTree {    // RUPQ variant is a simple extension of PURQ
+public:
+  RUPQ(int n) : FenwickTree(n) {}
+  int point_query(int i) { return rsq(i); }
+  void range_update(int i, int j, int v) { update(i, v), update(j+1, -v); }
+};
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    // cin.ignore(); must be there when using getline(cin, s)
+    n++;
+    vi sumas(n,0);
+    vi nums(n,0);
+    for (int i = 1; i < n; i++) {
+        cin >> nums[i];
+        sumas[i]=i;
+    }
+    FenwickTree fen = FenwickTree(sumas);
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     cout << fen.rsq(i) << ' ';
+    // }
+    // for (auto i = nums.rbegin(); i != nums.rend(); ++i)
+    // {
+    //     cout << *i << endl;
+    // }
+    // for (int i = 0; i < 10; ++i)
+    // {
+    //     cout <<' '<<  fen.select(i);
+    // }
+    // cout << endl;
+    // for (auto i = nums.rbegin(); i != nums.rend(); ++i)
+    // {
+    //     int re = fen.select(*i);
+    //     fen.update(re,-re);
+    //     cout << re << ',' << *i << endl;
+    //     for (int aa = 0; aa < n; ++aa)
+    // {
+    //     cout <<' '<<  fen.rsq(aa);
+    // }
+    // cout << endl;
+    // }
+    vi resp(n,0);
+    for (int i = 1; i < n; ++i)
+    {
+        int re = fen.select(nums[n-i]);
+        fen.update(re,-re);
+        resp[n-i] = re;
+    }
+    cout << resp[1];
+    for (int i = 2; i < n; ++i)
+    {
+        cout << ' ' << resp[i];
+    }
+    cout << endl;
+    return 0;
+}
